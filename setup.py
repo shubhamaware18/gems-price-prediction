0c1ef7383d0098fb7d8acf5c10f83ffc91fcb7a5
Shubham Aware <sandyaware07@gmail.com>
2024-01-31 19:45:29 +0530

    First Commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Thisn is the demo project developed o n the concept of MLOps',
+    description='This is the demo project developed o n the concept of MLOps',
     author='Shubham Aware',
     license='',
 )
