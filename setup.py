9fe5ffbf5c4647f5f260557543fcae82fb0eebdf
Shubham Aware <sandyaware07@gmail.com>
2024-02-02 20:22:38 +0530

    setu file updated

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import find_packages, setup
+from typing import List
+
+"""HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements"""
+
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='This is the demo project developed o n the concept of MLOps',
+    name='DimondPricePrediction',
+    version='0.1.0', 
+    description='This is the demo project developed on the concept of MLOps',
     author='Shubham Aware',
-    license='',
+    author_email='awareshubham1996@gmail.com',
+    install_requires=["scikit-learn", "pandas", "numpy"],
+    packages=find_packages(),
+    license=''
 )
