0b9282037e9e4cbfe188168c6fd6efff4b770874
Shubham Aware <sandyaware07@gmail.com>
2024-02-04 21:53:05 +0530

    commented code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -44,6 +44,7 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
+        # except logic
         except Exception as e:
             logging.error(str(e))
             raise customexception(e, sys)
